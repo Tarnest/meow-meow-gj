9a810e29e819567ebfede348bf8653f636d89c5b
Tarnest <tristanconnolly1@gmail.com>
2026-02-13 20:43:57 -0500

    Added Gravity and Jumping

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+var gravity: float = 5000
+var speed: int = 300
+var jump_speed: int = 1200
+var direction_x: int = 0
+
+func _physics_process(delta: float) -> void:
+	direction_x = 0
+	
+	if Input.is_action_pressed("move_left"):
+		direction_x += -1
+	if Input.is_action_pressed("move_right"):
+		direction_x += 1
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_speed
+	
+	velocity.x = direction_x * speed
+	velocity.y += gravity * delta
+	move_and_slide()
