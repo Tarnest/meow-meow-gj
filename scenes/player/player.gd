d458412405c7c830ee0e0272949bb0ebf8ebd098
Tarnest <tristanconnolly1@gmail.com>
2026-02-14 11:47:57 -0500

    Added Prelimary Platforms and Changed Player Jump

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,10 +2,13 @@ extends CharacterBody2D
 
 @onready var interaction: Area2D = %Interaction
 
-var gravity: float = 3000
-var speed: int = 300
-var jump_speed: int = 600
+var gravity: float = 20
+var speed: int = 250
 var direction_x: int = 0
+
+var jump_speed: int = 320
+var jump_timer: float = 0.0
+var jump_velocity: float = 0.0
 
 func _physics_process(delta: float) -> void:
 	direction_x = 0
@@ -14,8 +17,6 @@ func _physics_process(delta: float) -> void:
 		direction_x += -1
 	if Input.is_action_pressed("move_right"):
 		direction_x += 1
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -jump_speed
 	if Input.is_action_just_pressed("interact"):
 		var areas: Array[Area2D] = interaction.get_overlapping_areas()
 		for area in areas:
@@ -23,6 +24,18 @@ func _physics_process(delta: float) -> void:
 				var interactable: Interactable = area
 				interactable.interact()
 	
-	velocity.x = direction_x * speed
-	velocity.y += gravity * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		jump_velocity = -jump_speed - abs(direction_x * speed) / 3
+	if Input.is_action_pressed("jump") and jump_timer < 0.1:
+		velocity.y = jump_velocity
+		jump_timer += delta
+	if Input.is_action_just_released("jump"):
+		jump_timer += 10
+	if !Input.is_action_pressed("jump") and is_on_floor():
+		jump_timer = 0
+		jump_velocity = 0
+	if !is_on_floor():
+		velocity.y += gravity
+	
+	velocity.x = lerpf(velocity.x, direction_x * speed, 0.1)
 	move_and_slide()
