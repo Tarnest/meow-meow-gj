a6c3c8e158e809493a5e7f3324d1eee10d26fc76
Tarnest <tristanconnolly1@gmail.com>
2026-02-15 11:18:24 -0500

    Changed Main Scene and Added Walls to Shop

--- scenes/start/exit_button.gd
+++ scenes/start/exit_button.gd
@@ -4,11 +4,6 @@ extends Button
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pressed.connect(_on_button_pressed)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _on_button_pressed() -> void:
 	get_tree().quit()
